397fe01af5edd22c3ee5628cb328c970a04cc806
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2024-05-22 11:55:31 +0200

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -78,6 +78,7 @@ def main():
           f"name={row[0]}; email={row[1]}; phone={row[2]}; "
           f"ssn={row[3]}; password={row[4]}; ip={row[5]}; "
           f"last_login={row[6]}; user_agent={row[7]};"
+        )
         logger.info(msg)
     cursor.close()
     db.close()
