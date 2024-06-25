2a0eeec7e511d3f1c74182f11183fa413171cca9
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2024-06-25 10:34:09 +0200

    Update 2-app.py

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -23,6 +23,7 @@ def get_locale():
     """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """Home page"""
