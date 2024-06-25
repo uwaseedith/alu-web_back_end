1a707a7c557fe6f921ecdf7a71495705f3605b13
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2024-06-25 10:30:50 +0200

    okay

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -25,7 +25,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-
+@babel.localeselector
 def get_locale():
     """Get locale for your application"""
     locale = request.args.get('locale')
@@ -36,7 +36,7 @@ def get_locale():
             and user["locale"] in app.config['LANGUAGES']):
         return g.user["locale"]
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
